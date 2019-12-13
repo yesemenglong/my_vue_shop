e570bafa4fb364680f44144570f14cef9665de44
yesemonglong <1981376124@qq.com>
2019-12-13 18:52:02 +0800

    图片

--- src/main.js
+++ src/main.js
@@ -9,6 +9,12 @@ import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
 import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
+// 导入富文本编辑器
+import VueQuilEditor from 'vue-quill-editor'
+// require styles 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
@@ -22,6 +28,8 @@ axios.interceptors.request.use(config => {
 })
 Vue.prototype.$http = axios
 Vue.component('tree-table', TreeTable)
+// 富文本编辑器，注册为全局可用的组件
+
 Vue.filter('dateFormat', function (originVal) {
   const dt = new Date(originVal)
 
