fa963a4539aa59175c70ebb9ad320c88d53d9e76
yesemonglong <1981376124@qq.com>
2019-12-16 16:56:22 +0800

    '完成商品功能开发'

--- src/main.js
+++ src/main.js
@@ -9,6 +9,12 @@ import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
 import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// require styles 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
@@ -22,6 +28,9 @@ axios.interceptors.request.use(config => {
 })
 Vue.prototype.$http = axios
 Vue.component('tree-table', TreeTable)
+// 富文本编辑器，注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
 Vue.filter('dateFormat', function (originVal) {
   const dt = new Date(originVal)
 
@@ -30,8 +39,8 @@ Vue.filter('dateFormat', function (originVal) {
   const d = (dt.getDate() + '').padStart(2, '0')
 
   const hh = (dt.getHours() + '').padStart(2, '0')
-  const mm = (dt.getMinutes() + '').padStart(2, 0)
-  const ss = (dt.getSeconds() + '').padStart(2, 0)
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
 
   return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
 })
